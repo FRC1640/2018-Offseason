72e98b8c3007fd789ef0b029fc732a8822dc5367
David Gentzel <dgentzel1@gmail.com>
2018-12-16 19:10:35 -0500

    Added buttons for arm class (untested)

--- src/main/java/frc/team1640/robot/ArmController.java
+++ src/main/java/frc/team1640/robot/ArmController.java
@@ -2,31 +2,46 @@ package frc.team1640.robot;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import frc.team1640.robot.Arm.ArmPosition;
+import frc.team1640.robot.Controller;
 
 public class ArmController {
 
-    double speed;
-    Solenoid armBrake;
-    OperatingMode operatingMode;
-    ArmState armState;
-    ArmPosition armTarget;
+    private Controller armController; 
 
-    public static enum OperatingMode {
-		Manual,
-        Managed;
+    public boolean armController (Controller armController) {
+        if (armController.controller.getAButton()) {
+			Arm.setPreset(ArmPosition.Floor);
+		}
+		else if (armController.controller.getBButton()) {
+			Arm.setPreset(ArmPosition.Switch);
+        }
+        else if (armController.controller.getYButton()) {
+            Arm.setPreset(ArmPosition.Switch);
+        }
+
+
+    // double speed;
+    // Solenoid armBrake;
+    // OperatingMode operatingMode;
+    // ArmState armState;
+    // ArmPosition armTarget;
+
+    // public static enum OperatingMode {
+	// 	Manual,
+    //     Managed;
     
-    }
+    // }
 
-    public static enum ArmState {
-        AT_TARGET,  
-        TRANSITIONING;
+    // public static enum ArmState {
+    //     AT_TARGET,  
+    //     TRANSITIONING;
 
-    }
+    // }
 
-    public void IncreaseAngle() {
-        int ord = armTarget.ordinal();
+    // public void IncreaseAngle() {
+    //     int ord = armTarget.ordinal();
         
 
-    }
+    // }
 
 }
